8296b6f4d9198217aa13f472e7f7dcb05f85d0de
Miroslav Murin <miroslavmurin3@gmail.com>
2022-12-12 22:05:20 +0100

    benchmark

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
@@ -24,10 +25,6 @@ namespace Maui.Controls.Sample
 				flxlayout.Add(button);
 			}
 
-
-
-
-			
 			Stopwatch timer = new Stopwatch();
 			timer.Start();
 
@@ -37,32 +34,80 @@ namespace Maui.Controls.Sample
 
 			TimeSpan timeTaken = timer.Elapsed;
 			Debug.WriteLine("*** page Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
-			
-			
-
-			/*
-		for (int i = 0; i < 20000; i++)
-		{
-			flxlayout2.Add(new Button());
-		}
 
 
-		Stopwatch timer2 = new Stopwatch();
-		timer2.Start();
-
-
-		flxlayout.Children.Clear();
-
-		timer2.Stop();
-
-		TimeSpan timeTaken2 = timer2.Elapsed;
-		Debug.WriteLine("*** Time taken: " + timeTaken2.ToString(@"m\:ss\.fff"));
-			*/
+			//BenchmarkStrings();
 
 		}
-		void TestFunction()
-		{
 
+
+		void BenchmarkStrings()
+		{
+			string x = "Par1 test";
+			string y = "Par2 test";
+
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				Debug.WriteLine("Benchmark test par1: {0}  par2: {1}", x, y);
+			}
+
+			timer.Stop();
+			TimeSpan timeTaken = timer.Elapsed;
+			Debug.WriteLine("Composite formatting - Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+
+			timer.Reset();
+			timer.Start();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				Debug.WriteLine($"Benchmark test par1: {x}  par2: {y}");
+			}
+
+			timer.Stop();
+			timeTaken = timer.Elapsed;
+			Debug.WriteLine("String interpolation - Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+
+
+			timer.Reset();
+			timer.Start();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				Debug.WriteLine(string.Format("Benchmark test par1: %s  par2: %s", x, y));
+			}
+
+			timer.Stop();
+			timeTaken = timer.Elapsed;
+			Debug.WriteLine("String Format - Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+
+
+			timer.Reset();
+			timer.Start();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				Debug.WriteLine("Benchmark test par1: " + x + "  par2: " + y);
+			}
+
+			timer.Stop();
+			timeTaken = timer.Elapsed;
+			Debug.WriteLine("String + String - Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
+
+
+			timer.Reset();
+			timer.Start();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				Debug.WriteLine(new StringBuilder().AppendFormat("Benchmark test par1: {0}  par2: {1}", x, y).ToString());
+			}
+
+			timer.Stop();
+			timeTaken = timer.Elapsed;
+			Debug.WriteLine("StringBuilder - Time taken: " + timeTaken.ToString(@"m\:ss\.fff"));
 		}
 	}
 }
